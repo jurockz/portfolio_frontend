dde1b3d4ec0112a2c6a0a6bef8182e7566938510
jurek.ohrndorf <jurek.ohrndorf@check24.de>
2024-09-14 02:08:17 +0200

    add some padding

--- src/styles/Introduction.styled.ts
+++ src/styles/Introduction.styled.ts
@@ -13,7 +13,7 @@ export const StyledSection = styled.section<StyledSectionI>`
   align-items: center;
   background: ${(props) => props.colorb || "#fff"};
   @media (max-width: 880px) {
-    padding: 20px 20px 0px 20px;
+    padding: 50px 20px 0px 20px;
   }
 `;
 
